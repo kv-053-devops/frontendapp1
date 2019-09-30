2e21ede9e5e26eab24a739a6498e08cc78210d6a
tt-softs <52212176+tt-softs@users.noreply.github.com>
2019-09-30 14:15:16 +0300

    Update server_flask.py

--- server_flask.py
+++ server_flask.py
@@ -165,7 +165,7 @@ def settings():
 
 @app.route('/hello')
 def hello_world():
-    return 'Task for flask-StockApp-v2'
+    return 'Task for flask-StockApp-v2.1'
 
 
 @app.route('/api/add', methods=['POST'])
